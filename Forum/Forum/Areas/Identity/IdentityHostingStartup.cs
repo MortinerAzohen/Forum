1d3dded94c82579465acd646c6abb545ede2ffca
Michał Siciński <mortiner1@o2.pl>
2020-07-15 19:46:37 +0200

    initial

---
+++ Forum/Forum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Forum.Areas.Identity.Data;
+using Forum.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Forum.Areas.Identity.IdentityHostingStartup))]
+namespace Forum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ForumDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ForumDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ForumDbContext>();
+            });
+        }
+    }
+}
---
+++ Forum/Forum/Models/Forum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Forum.Models
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedMyProperty { get; set; }
+        public string ImageUrl { get; set; }
+        public virtual IEnumerable<Post> Posts { get; set; }
+
+    }
+}
